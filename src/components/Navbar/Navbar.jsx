98a91bc3766589942bd056cec939847dbf5ca52b
unknown <benguiratoussama1998@gmail.com>
2024-03-09 18:09:54 +0100

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="google-keep-logo.png" height="100px" width="100px" />
+    </div>
+  );
+}
+
+export default Navbar;
