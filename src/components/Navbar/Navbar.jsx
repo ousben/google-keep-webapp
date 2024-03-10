f058031c21071516f33099ef4484f55fe7fdfb78
unknown <benguiratoussama1998@gmail.com>
2024-03-10 13:58:31 +0100

    Navbar components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <img src="google-keep-logo.png" height="100px" width="100px" />
-    </div>
+    <header>
+      <div className="navbar">
+        <img src="google-keep-logo.png" height="100px" width="100px" />
+      </div>
+    </header>
   );
 }
 
