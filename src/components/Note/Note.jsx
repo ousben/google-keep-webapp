5cc44706fa2805dba44068eb290652cefbc6987c
unknown <benguiratoussama1998@gmail.com>
2024-03-12 20:27:34 +0100

    Note component

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -4,15 +4,17 @@ import "./Note.css";
 function Note() {
   const [text, setText] = useState("");
   const [size, setSize] = useState("small-note");
+  const [readOnlyValue, setReadOnlyValue] = useState(true);
 
   const handleClick = () => {
     setSize(size==="small-note" ? "large-note" : "small-note");
+    setReadOnlyValue(readOnlyValue === true ? false : true);
   }
 
   return (
     <div className={size} onClick={handleClick}>
-      <h1>title</h1>
-      <textarea>{text}</textarea>
+      <input type="text" readOnly={readOnlyValue} />
+      <textarea id="textarea" readOnly={readOnlyValue}>{text}</textarea>
     </div>
   );
 }
